2848024a1b2fbd27c111a344bebd13e0e06429f4
Ben Harper <rogojin@gmail.com>
2019-10-10 10:45:43 +0200

    authenticated HTTP handlers

--- http.go
+++ http.go
@@ -5,20 +5,26 @@ import (
 	"fmt"
 	"net/http"
 	"strconv"
+	"time"
 
+	"github.com/IMQS/serviceauth"
+	"github.com/IMQS/serviceauth/permissions"
 	"github.com/julienschmidt/httprouter"
 )
 
+// AuthenticatedHandler is an HTTP handler function that has already had authentication information read from the auth service
+type AuthenticatedHandler func(w http.ResponseWriter, r *http.Request, p httprouter.Params, auth *serviceauth.Token)
+
 // RunProtected runs 'func' inside a panic handler that recognizes our special errors,
-// and sends the appropriate HTTP response.
-func RunProtected(w http.ResponseWriter, r *http.Request, p httprouter.Params, handler func(w http.ResponseWriter, r *http.Request, p httprouter.Params)) {
+// and sends the appropriate HTTP response if a panic does occur.
+func RunProtected(w http.ResponseWriter, handler func()) {
 	defer func() {
-		if r := recover(); r != nil {
-			if hErr, ok := r.(HTTPError); ok {
+		if rec := recover(); rec != nil {
+			if hErr, ok := rec.(HTTPError); ok {
 				http.Error(w, hErr.Message, hErr.Code)
-			} else if err, ok := r.(error); ok {
+			} else if err, ok := rec.(error); ok {
 				http.Error(w, err.Error(), http.StatusInternalServerError)
-			} else if err, ok := r.(string); ok {
+			} else if err, ok := rec.(string); ok {
 				http.Error(w, err, http.StatusInternalServerError)
 			} else {
 				http.Error(w, "Unrecognized panic", http.StatusInternalServerError)
@@ -26,35 +32,67 @@ func RunProtected(w http.ResponseWriter, r *http.Request, p httprouter.Params, h
 		}
 	}()
 
-	handler(w, r, p)
+	handler()
 }
 
-// Handle adds a protected HTTP route to router (ie handle will run inside RunProtected)
+// Handle adds a protected HTTP route to router (ie handle will run inside RunProtected, so you get a panic handler)
 func Handle(router *httprouter.Router, method, path string, handle httprouter.Handle) {
 	wrapper := func(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
-		RunProtected(w, r, p, handle)
+		RunProtected(w, func() { handle(w, r, p) })
 	}
 	router.Handle(method, path, wrapper)
 }
 
+// HandleAuthenticated adds a protected HTTP route to router (ie handle will run inside RunProtected, so you get a panic handler)
+// Any permission specified in needPermissions must be present in the authentication information, otherwise the handler
+// will not call your 'handle' function, but will return with 403 Forbidden.
+// In addition, the authentication token must have the 'enabled' permission set, otherwise a 403 Forbidden is returned, with
+// the response body "User Disabled"
+func HandleAuthenticated(router *httprouter.Router, method, path string, handle AuthenticatedHandler, needPermissions []int) {
+	wrapper := func(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+		authCode, authMsg, authToken := serviceauth.GetToken(r)
+		if authCode != http.StatusOK {
+			http.Error(w, authMsg, authCode)
+			return
+		}
+		if !authToken.HasPermByID(permissions.PermEnabled) {
+			http.Error(w, "User Disabled", http.StatusForbidden)
+			return
+		}
+		RunProtected(w, func() { handle(w, r, p, authToken) })
+	}
+	router.Handle(method, path, wrapper)
+}
+
+// ParseID parses a 64-bit integer, and returns zero on failure
 func ParseID(s string) int64 {
 	id, _ := strconv.ParseInt(s, 10, 64)
 	return id
 }
 
-func SendJson(w http.ResponseWriter, obj interface{}) {
+// SendJSON encodes 'obj' to JSON, and sends it as an HTTP application/json response
+func SendJSON(w http.ResponseWriter, obj interface{}) {
 	w.Header().Set("Content-Type", "application/json")
 	b, err := json.Marshal(obj)
 	Check(err)
 	w.Write(b)
 }
 
+// SendID encodes 'id' as a string, and sends it as an HTTP text/plain response
 func SendID(w http.ResponseWriter, id interface{}) {
 	w.Header().Set("Content-Type", "text/plain")
 	fmt.Fprintf(w, "%v", id)
 }
 
+// SendOK sends "OK" as a text/plain response
 func SendOK(w http.ResponseWriter) {
 	w.Header().Set("Content-Type", "text/plain")
 	w.Write([]byte("OK"))
+}
+
+// SendPong sends a reply to an HTTP ping request, which checks if the service is alive
+func SendPong(w http.ResponseWriter) {
+	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Cache-Control", "max-age=0, no-cache")
+	fmt.Fprintf(w, `{"Timestamp": %v}`, time.Now().Unix())
 }
---
+++ service.go
@@ -0,0 +1,13 @@
+package nf
+
+import (
+	"github.com/IMQS/gowinsvc/service"
+)
+
+// RunService runs 'run' as a service on Windows, but if that fails, then falls back to running in the foreground
+func RunService(run func()) {
+	if !service.RunAsService(run) {
+		// Run in the foreground
+		run()
+	}
+}
