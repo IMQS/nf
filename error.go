8cb8420fed6fb6a82df041117f603a4c701258a0
Ben Harper <rogojin@gmail.com>
2019-10-17 15:14:11 +0200

    Add more HTTP Panic methods

--- error.go
+++ error.go
@@ -1,6 +1,7 @@
 package nf
 
 import (
+	"fmt"
 	"net/http"
 )
 
@@ -16,9 +17,34 @@ func Panic(code int, message string) {
 	panic(HTTPError{code, message})
 }
 
-// PanicForbidden panics with a 400 Forbidden.
+// PanicBadRequest panics with a 400 Bad Request.
+func PanicBadRequest() {
+	panic(HTTPError{http.StatusBadRequest, "Bad Request"})
+}
+
+// PanicBadRequestf panics with a 400 Bad Request.
+func PanicBadRequestf(format string, args ...interface{}) {
+	panic(HTTPError{http.StatusBadRequest, fmt.Sprintf(format, args...)})
+}
+
+// PanicForbidden panics with a 403 Forbidden.
 func PanicForbidden() {
 	panic(HTTPError{http.StatusForbidden, "Forbidden"})
+}
+
+// PanicNotFound panics with a 404 Not Found.
+func PanicNotFound() {
+	panic(HTTPError{http.StatusNotFound, "Not Found"})
+}
+
+// PanicServerError panics with a 500 Internal Server Error
+func PanicServerError(msg string) {
+	panic(HTTPError{http.StatusInternalServerError, msg})
+}
+
+// PanicServerErrorf panics with a 500 Internal Server Error
+func PanicServerErrorf(format string, args ...interface{}) {
+	panic(HTTPError{http.StatusInternalServerError, fmt.Sprintf(format, args...)})
 }
 
 // Check causes a panic if err is not nil.
