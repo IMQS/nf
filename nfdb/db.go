2d42e8eabb0dbc915a3e3c460902f027d8f9ec1e
FritzOnFire <fritz.engl@gmail.com>
2019-10-23 13:35:26 +0200

    Fixed Model struct tag

--- nfdb/db.go
+++ nfdb/db.go
@@ -25,7 +25,7 @@ const (
 // Model is our base class for a GORM model.
 // The default GORM Model uses int, but we prefer int64
 type Model struct {
-	ID        *int64     `json:"id",gorm:"primary_key"`
+	ID        *int64     `json:"id" gorm:"primary_key"`
 	CreatedAt *time.Time `json:"createdAt"`
 	UpdatedAt *time.Time `json:"updatedAt"`
 	DeletedAt *time.Time `json:"deletedAt"`
