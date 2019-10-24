50b9f9d2622b4af916c21701ef4a8f5cc99cd57a
Ben Harper <rogojin@gmail.com>
2019-10-24 11:49:09 +0200

    Create a static file handler

--- service.go
+++ service.go
@@ -1,8 +1,17 @@
 package nf
 
 import (
+	"os"
+
 	"github.com/IMQS/gowinsvc/service"
 )
+
+// IsRunningInContainer returns true if we're running inside a docker container.
+func IsRunningInContainer() bool {
+	_, err := os.Stat("/.dockerenv")
+	return err == nil
+
+}
 
 // RunService runs 'run' as a service on Windows, but if that fails, then falls back to running in the foreground.
 func RunService(run func()) {
