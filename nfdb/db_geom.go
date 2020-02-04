8b89cdcdcf43585c6fbdb4513de99ad18bfcb9d5
Jason K. George <jasonkofo@gmail.com>
2020-02-04 14:38:52 +0200

    Adds GetGeoJSON function to db.
    Adds some tests to test GetGeoJSON and GetGeometryColumn.

---
+++ nfdb/db_geom.go
@@ -0,0 +1,80 @@
+package nfdb
+
+import (
+	"encoding/hex"
+	"errors"
+	"fmt"
+	"reflect"
+
+	"github.com/jinzhu/gorm"
+	"github.com/twpayne/go-geom/encoding/ewkb"
+	"github.com/twpayne/go-geom/encoding/geojson"
+)
+
+// GetGeometryColumn returns the name of the geometry column in the GORM struct.
+// Returns the columnName and an error
+func GetGeometryColumn(db *gorm.DB, model interface{}) (*string, error) {
+	r := reflect.ValueOf(model).Elem().Type()
+	tableName := r.Name()
+
+	res := struct {
+		FGeometryColumn string
+	}{}
+
+	err := db.Raw(
+		`SELECT "f_geometry_column"
+		 FROM "geometry_columns"
+		 WHERE "f_table_name" = ?`,
+		gorm.TheNamingStrategy.ColumnName(tableName),
+	).Scan(&res).Error
+	if err != nil {
+		if err.Error() == "record not found" {
+			return nil, errors.New("No geometry field found")
+		}
+		return nil, err
+	}
+
+	columnName := ""
+	for i := 0; i < r.NumField(); i++ {
+		tableColumnName := gorm.TheNamingStrategy.ColumnName(r.Field(i).Name)
+
+		if tableColumnName == res.FGeometryColumn {
+			columnName = r.Field(i).Name
+			break
+		}
+	}
+
+	if columnName == "" {
+		return nil, fmt.Errorf("Could not find matching geometry field in GORM struct")
+	}
+
+	return &columnName, nil
+}
+
+// GetGeoJSON marshals the geometry associated with the model into a GeoJSON
+// geometry and returns it.
+func GetGeoJSON(db *gorm.DB, model interface{}) (*geojson.Geometry, error) {
+	geomColumn, err := GetGeometryColumn(db, model)
+	if err != nil {
+		return nil, fmt.Errorf("while getting the name of the geometry column of GORM struct: %v", err)
+	}
+
+	r := reflect.ValueOf(model).Elem()
+	strGeom := r.FieldByName(*geomColumn).String()
+	rawGeom, err := hex.DecodeString(strGeom)
+	if err != nil {
+		return nil, fmt.Errorf("while decoding geom string: %v", err)
+	}
+
+	geom, err := ewkb.Unmarshal(rawGeom)
+	if err != nil {
+		return nil, err
+	}
+
+	geoJSON, err := geojson.Encode(geom)
+	if err != nil {
+		return nil, err
+	}
+
+	return geoJSON, nil
+}
--- nfdb/db_test.go
+++ nfdb/db_test.go
@@ -1,10 +1,77 @@
 package nfdb
 
 import (
+	"encoding/json"
+	"strings"
 	"testing"
+	"unicode"
 
+	"github.com/IMQS/log"
+	"github.com/jinzhu/gorm"
 	"gotest.tools/assert"
 )
+
+const (
+	DBHOST     = "localhost"
+	DBUSER     = "unit_test_user"
+	DBPASSWORD = "unit_test_password"
+	DBNAME     = "nftest"
+)
+
+type PolyTable struct {
+	BaseModel
+	Geometry string
+}
+
+func DSN() string {
+	return "host=" + DBHOST +
+		" user=" + DBUSER +
+		" password=" + DBPASSWORD +
+		" dbname=" + DBNAME +
+		" sslmode=disable"
+}
+
+func CreateTestMigrations() []string {
+	return []string{`
+		CREATE EXTENSION IF NOT EXISTS postgis;
+
+		CREATE TABLE "poly_table"
+		(
+			"id" BIGSERIAL PRIMARY KEY,
+			"geometry" geometry(Polygon,4326)
+		)
+	`}
+}
+
+func CreateTestDB(t *testing.T) *gorm.DB {
+	var flags DBConnectFlags
+	flags |= DBConnectFlagWipeDB
+	log := log.New(log.Stdout)
+	db, err := OpenDB(
+		log,
+		"postgres",
+		DSN(),
+		MakeMigrations(log, CreateTestMigrations()),
+		flags,
+	)
+	if err != nil {
+		t.Fatal(err.Error())
+	}
+
+	return db
+}
+
+// RemoveWhitespace removes whitespace from strings
+func RemoveWhitespace(str string) string {
+	var b strings.Builder
+	b.Grow(len(str))
+	for _, ch := range str {
+		if !unicode.IsSpace(ch) {
+			b.WriteRune(ch)
+		}
+	}
+	return b.String()
+}
 
 func TestSQL(t *testing.T) {
 	verify := func(in, out string) {
@@ -19,4 +86,70 @@ func TestSQL(t *testing.T) {
 	verify("a", "()")
 	verify("1,,3", "(1,3)")
 	verify(",", "()")
+}
+
+func TestGeom(t *testing.T) {
+	db := CreateTestDB(t)
+	defer db.Close()
+
+	geoJSON := RemoveWhitespace(`
+	{
+		"type": "Polygon",
+		"coordinates": [
+			[
+				[0.00013, 0.000131],
+				[0.000131, 0.00013],
+				[0.000129, 0.00013],
+				[0.00013, 0.000131]
+			]
+		]
+	}`)
+
+	err := db.Exec(`
+		INSERT INTO "poly_table" (
+			"geometry"
+		) VALUES (ST_SetSRID(ST_GeomFromGeoJSON(?),4326))
+	`, geoJSON).Error
+	if err != nil {
+		t.Fatal("Failed to insert geometry")
+	}
+
+	polys := []PolyTable{}
+
+	err = db.Find(&polys).Error
+	if err != nil {
+		t.Fatalf("Failed to find a poly in PolyTable: %v", err)
+	}
+	if len(polys) == 0 {
+		t.Fatalf("Failed to find a poly in PolyTable: No Row Returned")
+	}
+
+	poly := polys[0]
+
+	// Test getting the geometry column
+
+	columnName, err := GetGeometryColumn(db, &poly)
+	if err != nil {
+		t.Fatalf("Failed to get the name of the geometry field from the GORM struct 'PolyTable': %v", err)
+	}
+
+	if *columnName != "Geometry" {
+		t.Fatalf("Unexpected geometry field name for GORM struct 'PolyTable'. Expected name should be 'Geometry', got '%v'", *columnName)
+	}
+
+	// Test getting GetGeoJSON from a model
+
+	geometry, err := GetGeoJSON(db, &poly)
+	if err != nil {
+		t.Fatalf("Failed while getting GeoJSON: %v", err)
+	}
+
+	rawJSON, err := json.Marshal(geometry)
+	if err != nil {
+		t.Fatalf("Failed while marshaling the geometry received from the db: %v", err)
+	}
+
+	if string(rawJSON) != geoJSON {
+		t.Fatalf("Unexpected geometry value received from db. Expected value: %v, received value: %v", geoJSON, string(rawJSON))
+	}
 }
